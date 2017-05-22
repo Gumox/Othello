49712255000762fb30cdac8d7571705c8f66a40c
KAU <KAU@KAU-PC>
2017-05-22 17:28:04 +0900

    2017 05 22 GrayPoint 서치 구현
    putStone 구현 해야함

--- Othellro/src/Reversi.java
+++ Othellro/src/Reversi.java
@@ -21,67 +21,64 @@ public class Reversi {
 			}
 		}
 	}
-	public void checkL(int b,int c){ // b는 현재 차레의 말 c는  상대 말
-		for(int i = 7;i >= 0 ; i--){
-			for(int j = 7;j >= 0 ; j--){
-				if(arr[i][j] == b){  //7,7부터 찾기시작
-					point = j;       
-					while(j >= 0 ){	 // 왼쪽으로 -1씩 움직이며 Gp를 놓으장소를 찾는다						
-						j--;
-						if(arr[i][j] == b && arr[i][j-1] == c){
-							arr[i][j-1] = 3;
+
+	public void checkD(int b,int c){
+				for(int j = 0;j < 8 ; j++){
+					if(arr[i][j] == b){      // 0,0부터 찾기시작
+						while(i < 7){        // Y축으로 -1씩 움직이면서 Gp를 놓을 장소를 찾는다 		
+							i++;
+							if(arr[i][j] == c && arr[i+1][j] == 0){
+								arr[i+1][j] = 3;
+							}
+						}
+					j = 0;
+			}
+		}
+	}
+	
+	public void checkLD(int b,int c){
+		for(int i = 7; i >= 0 ; i--){
+			for(int j =0; j<8; j++){
+				if(i<7){
+					j = 0;
+				}
+				if(arr[i][j] == b){      // 0,0부터 찾기시작
+					while(j < 7){        // X축으로 +1 Y축으로 -1씩 움직이면서 Gp를 놓을 장소를 찾는다 		
+						i++;
+						j++;
+						if(arr[i][j] == c && arr[i-1][j+1] == 0){
+							arr[i-1][j+1] = 3;
 						}
 					}
+				}	
+				j = 0;
+			}
+		}				
+	}
+	public void checkRD(int b,int c){
+		for(int i = 0; i < 8 ; i++){
+			for(int j =0; j<8; j++){
+				if(i>0){
+					j = 0;
 				}
-				j = 0;               // j 값을 리셋 i에 -1 된 상태로 넘어간다	
+				if(arr[i][j] == b){      // 0,0부터 찾기시작
+					while(j < 7){        // X축으로 +1 Y축으로 -1씩 움직이면서 Gp를 놓을 장소를 찾는다 		
+						i++;
+						j++;
+						if(arr[i][j] == c && arr[i+1][j+1] == 0){
+							arr[i+1][j+1] = 3;
+						}
+					}
+				}	
+				j = 0;
 			}
-		}
-	}
-	public void checkU(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
-	}
-	public void checkD(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
-	}
-	public void checkRU(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
-	}
-	public void checkLU(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
-	}
-	public void checkRD(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
-	}
-	public void checkLD(int[][] a,int b){
-		for(int i = 0;i < 8 ; i++){
-			for(int j = 0;j < 8 ; j++){
-				
-			}
-		}
+		}				
 	}
 
-	private void putStone(){
-		
+	private void putStone(int x,int y,int player,int enemy){
+		if(arr[x][y] == 3){
+			
+		}
 	}
 
 }
